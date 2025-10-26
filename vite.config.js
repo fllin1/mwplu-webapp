162ab7590b0dcfe9491e31f20546fa7b903ad44e
Grey Panda <1florentlin@gmail.com>
2025-10-26 11:45:21 +0100

    fix[build]: applied the vite plugin devtools only for dev environment, correction of build issues

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,14 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+export default defineConfig(async ({ mode }) => ({
+  plugins: [
+    vue(),
+    // Only enable Vue DevTools in development mode
+    ...(mode === 'development' ? [(await import('vite-plugin-vue-devtools')).default()] : []),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
@@ -43,4 +46,4 @@ export default defineConfig({
   define: {
     __VUE_PROD_DEVTOOLS__: false,
   },
-})
+}))
